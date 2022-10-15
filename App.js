94865928cf6ded5277f7df8e208351995769c91d
jngnandan <jngnandan@gmail.com>
2022-10-15 15:59:25 +0100

    centered div

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Text, View, Picker } from 'react-native';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex flex-col justify-center items-center h-screen bg-blue-300 px-12">
+      <View className="bg-white rounded h-40 w-full flex flex-col justify-center items-center">
+      <Text className="text-2xl" >Capitals and Countries</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
